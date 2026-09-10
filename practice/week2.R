a256d99904c7f255bbaf786f050c5bd558b86879
Lauryn Chan <laurynchankx@gmail.com>
2026-09-10 13:48:02 -0400

    edit pipe syntax

--- practice/week2.R
+++ practice/week2.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(tidycensus)
+library(dplyr)
 
 data <- get_acs(geography = "county",
                 variables = c("B19013_001","B01003_001"),
@@ -42,12 +43,34 @@ my_variable <- "B25077_001"     # the variable code, in quotes
 ## Pulling the data
 
 
-nj_house <- get_acs(
+county_data <- get_acs(
   geography = "county",
-  variables = my_variable,
-  state = my_state,
+  variables = "B25077_001",
+  state = "NJ",
   year = 2023,
   survey = "acs5",
   output = "wide"
 )
 
+county_data <- county_data |>
+  mutate(moe_pct = B25077_001M / B25077_001E * 100)
+
+county_data |>
+  arrange(desc(moe_pct)) |> `
+  head(10)
+
+library(ggplot2)
+
+ggplot(county_data, aes(x = moe_pct)) +
+  geom_histogram()
+
+
+install.packages("classInt")  # only need to do this once
+library(classInt)
+jenks <- classInt::classIntervals(
+  county_data$moe_pct,
+  n = 2,
+  style = "jenks"
+)
+
+jenks$brks
